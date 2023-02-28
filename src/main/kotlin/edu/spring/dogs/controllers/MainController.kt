a20c35d2ff626279be4befb05e905fc5770e2eff
Adam <you@example.com>
2023-02-28 18:01:47 +0100

    fin

--- src/main/kotlin/edu/spring/dogs/controllers/MainController.kt
+++ src/main/kotlin/edu/spring/dogs/controllers/MainController.kt
@@ -1,13 +1,32 @@
 package edu.spring.dogs.controllers
 
+import edu.spring.dogs.entities.Master
+import edu.spring.dogs.repositories.MasterRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.view.RedirectView
 
 
 @Controller
-@RequestMapping("/")
 class MainController {
-    @RequestMapping(path = ["","index"])
-    fun indexAction():String="index"
+
+    @Autowired
+    lateinit var masterRepository: MasterRepository
+
+    @RequestMapping("/")
+    fun indexAction(model: ModelMap):String{
+        model["masters"] = masterRepository.findAll()
+        return "index"
+    }
+
+    @PostMapping("/master/add")
+    fun submitNewAction(@ModelAttribute master: Master): RedirectView {
+        masterRepository.save(master)
+        return RedirectView("/")
+    }
 
 }
